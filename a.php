54d2d2ec24affbd7072e5707385aee5f7a356c98
nhbaby <nhbaby@gmail.com>
2016-01-14 18:52:44 +0900

    Update a.php

--- a.php
+++ a.php
@@ -2,7 +2,7 @@
 //print_r($_SERVER);
 echo "server ipo : " . $_SERVER['SERVER_ADDR'];
 echo "<br />";
-echo "now time : " . date(''Y-m-d H:i:s);
+echo "now time : " . date('Y-m-d H:i:s');
 echo "<br />";
 echo "hello!!!!";
 ?>
